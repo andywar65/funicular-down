abbe226a176d286fdcd9e57a63a8958de04ec2ed
andywar65 <andy.war1965@gmail.com>
2025-02-03 22:02:35 +0100

    saving entry

--- src/funicular_down/views.py
+++ src/funicular_down/views.py
@@ -1,7 +1,12 @@
+from io import BytesIO
+
 import requests
 from django.conf import settings
+from django.core.files import File
 from django.views.generic import TemplateView
 from requests.exceptions import JSONDecodeError
+
+from .models import Entry
 
 
 class ControlTemplateView(TemplateView):
@@ -14,6 +19,7 @@ class GetStatusTemplateView(TemplateView):
     def get_context_data(self, **kwargs):
         headers = {"Authorization": f"Token {settings.FUNICULAR_TOKEN}"}
         context = super().get_context_data(**kwargs)
+        context["status"] = ""
         r = requests.get(
             f"{settings.FUNICULAR_HOST}/pics/status/",
             headers=headers,
@@ -23,8 +29,13 @@ class GetStatusTemplateView(TemplateView):
         except JSONDecodeError:
             context["status"] = f"JSON encode error - {r.text}"
         for id, url in uploaded.items():
+            name = url.split("/")[-1]
             r = requests.get(
                 f"{settings.FUNICULAR_HOST}{url}",
             )
-        context["status"] = uploaded
+            e = Entry()
+            e.id_up = int(id)
+            e.image.save(name, File(BytesIO(r.content)))
+            e.save()
+            context["status"] += f"Created {id} - {name}\n"
         return context
