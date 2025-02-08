71e9328fb1b9999f3211caa1289d54ab78330023
andywar65 <andy.war1965@gmail.com>
2025-02-08 18:41:28 +0100

    add conditions for get status from server

--- src/funicular_down/models.py
+++ src/funicular_down/models.py
@@ -4,6 +4,7 @@ import requests
 from django.conf import settings
 from django.core.files import File
 from django.db import IntegrityError, models
+from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
 from requests.exceptions import JSONDecodeError
 
@@ -32,21 +33,32 @@ def get_status_from_server():
     except JSONDecodeError:
         status = f"<p>JSON encode error - {r.text}</p>"
     for id, data in uploaded.items():
-        name = data["url"].split("/")[-1]
-        r = requests.get(
-            f"{settings.FUNICULAR_HOST}{data["url"]}",
-        )
-        e = Entry()
-        e.id_up = int(id)
-        e.image.save(name, File(BytesIO(r.content)), save=False)
-        try:
-            e.save()
-            status += f"<p>Downloaded image {id} - {name}</p>"
-            downloaded = True
-        except IntegrityError:
-            status += f"<p>Already downloaded image {id}</p>"
-            downloaded = False
-        if downloaded:
+        if data["status"] == "UP":
+            name = data["url"].split("/")[-1]
+            r = requests.get(
+                f"{settings.FUNICULAR_HOST}{data["url"]}",
+            )
+            e = Entry()
+            e.id_up = int(id)
+            e.image.save(name, File(BytesIO(r.content)), save=False)
+            try:
+                e.save()
+                status += f"<p>Downloaded image {id} - {name}</p>"
+                downloaded = True
+            except IntegrityError:
+                status += f"<p>Already downloaded image {id}</p>"
+                downloaded = False
+            if downloaded:
+                r = requests.get(
+                    f"{settings.FUNICULAR_HOST}/pics/entry/{id}/downloaded/",
+                    headers=headers,
+                )
+                try:
+                    message = r.json()
+                    status += f"<p>{message["text"]}</p>"
+                except JSONDecodeError:
+                    status += f"<p>JSON encode error - {r.text}</p>"
+        elif data["status"] == "KI":
             r = requests.get(
                 f"{settings.FUNICULAR_HOST}/pics/entry/{id}/downloaded/",
                 headers=headers,
@@ -56,4 +68,14 @@ def get_status_from_server():
                 status += f"<p>{message["text"]}</p>"
             except JSONDecodeError:
                 status += f"<p>JSON encode error - {r.text}</p>"
+        elif data["status"] == "RQ":
+            entry = get_object_or_404(Entry, id_up=id)
+            files = {"file": open(entry.image.path, "rb")}
+            r = requests.put(
+                f"{settings.FUNICULAR_HOST}/pics/entry/{id}/uploaded/",
+                files=files,
+                data={"status": "ST"},
+                headers=headers,
+            )
+
     return status
